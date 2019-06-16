0e04b8ef24029a9973e9c42952984d46282765a3
Erik <erkizie@gmail.com>
2019-06-16 16:44:03 +0300

    Small changes in show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create] #Применяем только для отмеченныйх экшнов
 
   def index
     @articles = Article.all
