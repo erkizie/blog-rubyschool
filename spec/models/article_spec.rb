14b760de34ec8c01cf9320c3562a9e84d01e39af
Erik <erkizie@gmail.com>
2019-07-08 09:45:10 +0300

    Create tests for length validations for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,8 @@ describe Article do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
+    it { should validate_length_of :title }
+    it { should validate_length_of :text }
   end
 
   describe 'associations' do
