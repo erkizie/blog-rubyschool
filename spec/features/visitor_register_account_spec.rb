a9982c92a79c7b40d1e102bc4ec4edffe557f8ef
Erik <erkizie@gmail.com>
2019-07-09 12:01:18 +0300

    Made registration tests usable for other tests

--- spec/features/visitor_register_account_spec.rb
+++ spec/features/visitor_register_account_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 feature 'New account creation' do
   scenario 'allows visitor to create new account' do
-    visit new_user_registration_path
-
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'username_example'
-    fill_in :user_password, :with => 'password_example'
-    fill_in :user_password_confirmation, :with => 'password_example'
-
-    click_button 'Sign up'
-
+    sign_up
     expect(page).to have_content I18n.t 'devise.registrations.signed_up'
   end
+end
+
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'username_example'
+  fill_in :user_password, :with => 'password_example'
+  fill_in :user_password_confirmation, :with => 'password_example'
+
+  click_button 'Sign up'
+
 end
